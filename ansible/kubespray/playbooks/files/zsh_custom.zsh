3370c4564631d016897f9be0dedbb51f7ce176f1
2sangyun <sororiri@gmail.com>
2025-09-16 21:42:12 +0900

    zsh k8s 단축어 추가

--- ansible/kubespray/playbooks/files/zsh_custom.zsh
+++ ansible/kubespray/playbooks/files/zsh_custom.zsh
@@ -3,3 +3,41 @@ source <(kubectl completion zsh)
 
 alias g='git'
 
+alias dc='docker compose'
+
+
+
+
+# kubectl column
+kncolumn() {
+  local option="$1"
+  local optionValue=""
+  if [[ $option == "-opt="* ]]; then
+      optionValue="${option#*=}"
+      shift
+  else
+      option=""
+  fi
+
+  local printFields=""
+  for col in "$@"; do
+      printFields="${printFields}\$$col, "
+  done
+
+  # Remove trailing comma
+  printFields=${printFields%, }
+
+  local awkOptions="$optionValue {print $printFields}"
+  awk "$awkOptions" | column -t
+}
+
+
+# namespace 설정
+kn() {
+  if [ -z "$1" ]; then
+    echo "Usage: ksetns <namespace>"
+  else
+    kubectl config set-context --current --namespace="$1"
+    echo "Namespace set to '$1'"
+  fi
+}
